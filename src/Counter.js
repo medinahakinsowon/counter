fba28d71017edf8220b3b42eddce4d8ea843aa9e
medinat akinsowon <medinahakinsowon@gmail.com>
2023-08-15 14:04:53 +0100

    added button

--- src/Counter.js
+++ src/Counter.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const Counter = () => {
   const [counter, setcounter] = useState(0)
+  const [tabs, setTabs] = useState(true)
 
   const increment = () => {
     setcounter(counter + 1)
@@ -17,16 +18,32 @@ const Counter = () => {
     setcounter(0)
   }
 
+  const close = () => {
+    setTabs(prev => !prev)
+  }
+  const open = () =>{
+    setTabs(prev => prev)
+  }
   return (
     <div style={{ height: "100vh", backgroundColor: "#ddd" }}>
-      <div className="text-center" style={{ }}>
-        <h3 style={{ fontSize: "200px" }}>{counter}</h3>
+      <div>
+  
       </div>
-      <div className="text-center al">
-        <button className="btn btn-primary p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={increment}>Plus</button>
-        <button className="btn btn-dark p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={decrement}>Minus</button>
-        <button className="btn btn-danger p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={resetCounter}>Reset</button>
-      </div>
+      {tabs ? <div>
+        <button className="btn btn-primary p-3" style={{position:"relative", top:"30px", left: "30px"}} onClick={close}>close app</button>
+        <div className=" text-center">
+          <h3 style={{ fontSize: "200px" }}>{counter}</h3>
+        </div>
+        <div className="text-center al">
+          <button className="btn btn-primary p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={increment}>Plus</button>
+          <button className="btn btn-dark p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={decrement}>Minus</button>
+          <button className="btn btn-danger p-4 m-3" style={{ width: "150px", borderRadius: "10px" }} onClick={resetCounter}>Reset</button>
+        </div>
+      </div> : <div>
+        <h3>Application Closed</h3>
+        <button className="btn btn-danger" onClick={close}>Open App</button>
+      </div>}
+
     </div>
   )
 }
---
+++ src/FetchData.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+
+const FetchData = ()=>{
+   const fetchdata = async ()=>{
+       const response = await fetch()
+        const data = await response.json()
+        
+   }
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+
+export default FetchData;
